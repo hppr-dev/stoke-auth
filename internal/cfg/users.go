e26c20d30aaf299b0a4fcf30653f2f567ce9d709
Stephen Walker <stephen.d.a.walker@gmail.com>
2025-02-23 20:36:07 -0600

    Implement unmarshalling provider based on type.

--- internal/cfg/users.go
+++ internal/cfg/users.go
@@ -2,23 +2,18 @@ package cfg
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"stoke/internal/usr"
 )
 
-type foreignProvider interface {
-	UpdateUserClaims(username, password string, ctx context.Context) error
-}
 
-type ProviderConfig interface {
-	CreateProvider(context.Context) foreignProvider
-}
 
 type Users struct {
 	// Enable checking/creating stoke admin per-operation claims
 	CreateStokeClaims bool             `json:"create_stoke_claims"`
 	// Configs for providers
-	Providers         []ProviderConfig `json:"providers"`
-
+	Providers         []*ProviderConfig `json:"providers"`
 }
 
 func (u Users) withContext(ctx context.Context) context.Context {
@@ -28,5 +23,40 @@ func (u Users) withContext(ctx context.Context) context.Context {
 		providerList.CheckCreateForStokeClaims(ctx)
 	}
 
+	for _, prov := range u.Providers {
+		providerList.AddForeignProvider(prov.CreateProvider(ctx))
+	}
+
 	return providerList.WithContext(ctx)
+}
+
+type ProviderConfig struct {
+	providerConfig
+	ProviderType   string `json:"type"`
+}
+
+type providerConfig interface {
+	CreateProvider(context.Context) foreignProvider
+}
+
+type foreignProvider interface {
+	UpdateUserClaims(username, password string, ctx context.Context) error
+}
+
+func (pc *ProviderConfig) UnmarshalJSON(b []byte) error {
+	temp := struct {
+		ProviderType string `json:"type"`
+	}{}
+	err := json.Unmarshal(b, &temp)
+	if err != nil {
+		return err
+	}
+
+	pc.ProviderType = temp.ProviderType
+	switch(pc.ProviderType) {
+	case "ldap":
+		pc.providerConfig = &LDAPProviderConfig{}
+		return json.Unmarshal(b, pc.providerConfig)
+	}
+	return fmt.Errorf("Provider type not supported: %s", temp.ProviderType)
 }
